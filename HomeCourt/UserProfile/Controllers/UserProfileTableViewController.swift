950fab3a7ef2a495ea0c969fa2818f1f143110ee
Samuel Folledo <samuelfolledo@gmail.com>
2019-02-17 08:11:58 -0500

    still far

--- HomeCourt/UserProfile/Controllers/UserProfileTableViewController.swift
+++ HomeCourt/UserProfile/Controllers/UserProfileTableViewController.swift
@@ -7,84 +7,167 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class UserProfileTableViewController: UITableViewController {
 	
+	var data = [CellData]()
+	let cellId: String = "cellId"
+	var rows = 0
 	
-//MARK: Properties
+	@IBOutlet weak var backButton: UIBarButtonItem!
+	@IBOutlet weak var saveButton: UIBarButtonItem!
+	
 	
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-
 		
-    }
-
+		view.backgroundColor = .red
+		
+		//        data = [CellData.init(image: #imageLiteral(resourceName: "apple"), message: "About")] //create a new cell
+		//        data = [CellData.init(image: #imageLiteral(resourceName: "exit"), message: "Logout")] //create a new cell
+		//
+		
+		createDataCell()
+		
+		self.tableView.register(UserProfileCell.self, forCellReuseIdentifier: cellId)
+		
+		//but we still have to automatically make them resize to the contents inside of it
+		self.tableView.rowHeight = UITableView.automaticDimension
+		self.tableView.estimatedRowHeight = 100 //to make the cell have a limit and save memory //now in cellForRowAt layoutSubviews()
+		
+		
+	}
 	
-	// MARK: - Table view data source
-	override func numberOfSections(in tableView: UITableView) -> Int {
-		// #warning Incomplete implementation, return the number of sections
-		return 0
-	}
-
-	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		// #warning Incomplete implementation, return the number of rows
-		return 0
-	}
-
-	/*
-	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-		let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-		// Configure the cell...
-
-		return cell
-	}
-	*/
-
-	/*
-	// Override to support conditional editing of the table view.
-	override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-		// Return false if you do not want the specified item to be editable.
-		return true
-	}
-	*/
-
-	/*
-	// Override to support editing the table view.
-	override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-		if editingStyle == .delete {
-			// Delete the row from the data source
-			tableView.deleteRows(at: [indexPath], with: .fade)
-		} else if editingStyle == .insert {
-			// Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+	
+	
+	
+	func insertRowMode3(row: Int, cell: CellData, completion: @escaping ()-> Void) {
+		let indexPath = IndexPath(row: row, section: 0)
+		print("cell \(row) is appending at \(indexPath)")
+		data.append(cell)
+		tableView.insertRows(at: [indexPath], with: .right)
+		
+		DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+			completion()
 		}
 	}
-	*/
-
-	/*
-	// Override to support rearranging the table view.
-	override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+	
+	
+	func createDataCell() {
+		
+		let data1 = CellData.init(image: #imageLiteral(resourceName: "updateProfilePicture"), message: "Update Profile Picture")
+		let data2 = CellData.init(image: #imageLiteral(resourceName: "exit"), message: "Logout")
+		let data3 = CellData.init(image: #imageLiteral(resourceName: "apple"), message: "About")
+		
+		insertRowMode3(row: 0, cell: data1) {
+			self.insertRowMode3(row: 1, cell: data2, completion: {
+				self.insertRowMode3(row: 2, cell: data3, completion: {
+					print("Done inserting rows")
+				})
+			})
+		}
+		
+		//        data.append(data1)
+		//        data.append(data2)
+		//        data.append(data3)
+		
+		//        insertRowMode3(row: 0, cell: <#T##CellData#>, completion: <#T##() -> Void#>)
+		
 	}
-	*/
-
-	/*
-	// Override to support conditional rearranging of the table view.
-	override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-		// Return false if you do not want the item to be re-orderable.
-		return true
+	
+	
+	
+//	@IBAction func logoutTapped() {
+//		perform(#selector(handleLogout), with: nil, afterDelay: 0)
+//
+//	}
+	
+	
+	@IBAction func backTapped(_ sender: Any) {
+		dismiss(animated: true, completion: nil)
 	}
-	*/
-
-	/*
-	// MARK: - Navigation
-
-	// In a storyboard-based application, you will often want to do a little preparation before navigation
-	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-		// Get the new view controller using segue.destination.
-		// Pass the selected object to the new view controller.
+	
+	@IBAction func saveButtonTapped(_ sender: Any) {
+		
+		let alertAction = UIAlertAction(title: "Ok", style: .default) { (action) in
+			self.dismiss(animated: true, completion: nil)
+		}
+		
+		let alert = Service.showAlert(on: self, style: .alert, title: "Save Successfuly", message: "EEEEYYOOOOOOOOO!!!", actions: [alertAction]) //an alert that dismisses the VC once OK is pressed
+		self.present(alert, animated: true, completion: nil)
+		
 	}
-	*/
+	
+//didSelectRow
+	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		print(indexPath.row)
+		switch indexPath.row {
+		case 0:
+			//            handleUpdateImage()
+			print("Update image coming soon")
+		case 1:
+			handleLogout()
+		case 2:
+			print("About coming soon")
+		default:
+			break
+		}
+		
+	}
+	
+	
+//cellForRowAt
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! UserProfileCell
+		
+		cell.mainImage = data[indexPath.row].image
+		cell.message = data[indexPath.row].message
+		cell.layoutSubviews()
+		
+		return cell
+	}
+	
+	//numberOfRowsInSection
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return data.count
+	}
+	
+	
+	
+	
+	
+	
+	//handleLogout
+	@objc func handleLogout() {
+		let signoutAction = UIAlertAction(title: "Log Out?", style: .destructive) { (action) in
+			do {
+				try Auth.auth().signOut() //Signs user out
+				let cookies = HTTPCookieStorage.shared
+				let facebookCookies = cookies.cookies(for: URL(string: "https://facebook.com/")!)
+				for cookie in facebookCookies! {
+					cookies.deleteCookie(cookie )
+				}
+				let loginController = LoginViewController()
+				print("LOGGING OUTTTTTTTT")
+				self.present(loginController, animated: true, completion: nil)
+				
+			} catch let error { //present any error
+				let alert = Service.showAlert(on: self, style: .alert, title: "Logout Error", message: error.localizedDescription)
+				self.present(alert, animated: true, completion: nil)
+				//print(error)
+			}
+		}
+		
+		let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+		let signoutOrCancel: UIAlertController = Service.showAlert(on: self, style: .actionSheet, title: nil, message: nil, actions: [signoutAction, cancelAction], completion: nil)
+		self.present(signoutOrCancel, animated: true, completion: nil)
+		
+	}
+}
 
+struct CellData {
+	let image: UIImage?
+	let message: String?
 }
